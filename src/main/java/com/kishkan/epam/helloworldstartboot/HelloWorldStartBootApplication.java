24e5ec8afb28587e7d9feb2dfae5f3c5107c356b
Ivan <Kishkan91@gmail.com>
2019-10-31 19:21:26 +0400

    Initial commit

---
+++ src/main/java/com/kishkan/epam/helloworldstartboot/HelloWorldStartBootApplication.java
@@ -0,0 +1,13 @@
+package com.kishkan.epam.helloworldstartboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldStartBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldStartBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kishkan/epam/helloworldstartboot/HelloWorldStartBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kishkan.epam.helloworldstartboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldStartBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
